b0f7379b919ffdcbb5501811a5885287fe0c622f
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-09-18 14:48:50 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import AppMain from "./components/AppMain.vue";
+
+
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  components: {
+    AppMain
+  }
+
+}
+</script>
+
+<template>
+
+  <body>
+    <AppMain />
+
+  </body>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <h1>APP MAIN</h1>
+    </main>
+</template>
+
+<style scoped></style>
