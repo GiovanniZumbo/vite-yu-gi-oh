e3bbaa7510e29b07b303eefb80af6cd509c79e22
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-09-18 17:05:29 +0200

    Added props to maincard

--- src/components/MainCardList.vue
+++ src/components/MainCardList.vue
@@ -1,25 +1,53 @@
 <script>
+import axios from "axios";
 
 import MainCard from './MainCard.vue';
+import { store } from '../store';
 
 export default {
     data() {
         return {
+            cardList: [],
+            apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+            store
 
         }
     },
 
+    props: {
+
+    },
+
+    methods: {
+        getCards() {
+            axios.get(this.apiUrl)
+                .then((response) => {
+
+                    console.log(response);
+                    this.cardList = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
+
+
     components: {
         MainCard
+    },
+
+    created() {
+        this.getCards();
     }
 }
 </script>
 
 <template>
     <div class="row row-cols-5">
-        <div class="col">
-            <MainCard />
-        </div>
+
+        <MainCard v-for="card in cardList" :key="card.id" :cardObject="card" />
+
     </div>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -1,27 +1,12 @@
 import { reactive } from "vue";
-import axios from "axios";
 
 export const store = reactive({
 
 data(){
     return{
-        cardList:[],
-        apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
-
+cardList: []
     }
-},
-
-methods:{
-    getCards(){
-            axios.get(this.apiUrl)
-                    .then((response) => {
-
-                        console.log(response);
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    })
 }
 
-}
+
 })
